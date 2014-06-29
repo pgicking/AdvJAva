3454a1f97822b10ba77c818f613cea25ce17cd47
pgicking <pgicking@gmail.com>
2014-06-28 17:09:11 -0700

    Inital commit

---
+++ student/src/main/java/edu/pdx/cs410J/pgicking/Student.java
@@ -0,0 +1,62 @@
+package edu.pdx.cs410J.pgicking;
+
+import edu.pdx.cs410J.lang.Human;
+
+import java.util.ArrayList;
+                                                                                    
+/**                                                                                 
+ * This class is represents a <code>Student</code>.                                 
+ */                                                                                 
+public class Student extends Human {                                                
+   String name;
+   ArrayList classes;
+   double gpa;
+   String gender;
+
+    /**
+   * Creates a new <code>Student</code>                                             
+   *                                                                                
+   * @param name                                                                    
+   *        The student's name                                                      
+   * @param classes                                                                 
+   *        The names of the classes the student is taking.  A student              
+   *        may take zero or more classes.                                          
+   * @param gpa                                                                     
+   *        The student's grade point average                                       
+   * @param gender                                                                  
+   *        The student's gender ("male" or "female", case insensitive)             
+   */                                                                               
+  public Student(String name, ArrayList classes, double gpa, String gender) {
+      this.name = name;
+      this.classes = classes;
+      this.gpa = gpa;
+      this.gender = gender;
+  }
+
+  /**                                                                               
+   * All students say "This class is too much work"                                 
+   */
+  @Override
+  public String says() {                                                            
+        String says = "This Class is too much work";
+        return says;
+  }
+                                                                                    
+  /**                                                                               
+   * Returns a <code>String</code> that describes this                              
+   * <code>Student</code>.                                                          
+   */                                                                               
+  public String toString() {
+
+  }
+
+  /**
+   * Main program that parses the command line, creates a
+   * <code>Student</code>, and prints a description of the student to
+   * standard out by invoking its <code>toString</code> method.
+   */
+  public static void main(String[] args) {
+        if(args[0].isEmpty())
+            System.out.print("Missing command line arguments");
+  }
+}
